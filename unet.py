f574924449d3e0077b871b6a37dbea3c1de30b1d
Paul-Louis Pröve <mail@plpp.de>
2017-04-10 22:50:12 +0200

    quick fixes

--- unet.py
+++ unet.py
@@ -1,8 +1,8 @@
 
 # U-Net model for Keras with TF backend
 
-from keras.models import Input, Model, Conv2D, Concatenate
-from keras.models import MaxPooling2D, UpSampling2D, Dropout
+from keras.models import Input, Model
+from keras.layers import Conv2D, Concatenate, MaxPooling2D, UpSampling2D, Dropout
 
 def conv_block(m, dim, activation, dropout):
     m = Conv2D(dim, (3, 3), activation=activation, padding='same')(m)
@@ -18,7 +18,7 @@ def level_block(m, dim, depth, acti, drop):
         m = Concatenate(axis=1)([n, m])
     return conv_block(m, dim, activation, dropout)
 
-def U-Net(img_shape, start_dim=64, depth=4, activation='elu', dropout=0):
+def UNet(img_shape, start_dim=64, depth=4, activation='elu', dropout=0):
     i = Input(input_shape=img_shape)
     o = level_block(i, start_dim, depth, acti, drop)
     return Model(input=i, output=o)
