78b9eb2d8c6ab5074cbc13ec3de7b4abd2db2653
Paul-Louis Pröve <mail@plpp.de>
2017-04-12 16:50:05 +0200

    added missing up-conv layer & removed dropout

--- unet.py
+++ unet.py
@@ -1,25 +1,20 @@
-
-# U-Net model for Keras with TF backend
-
 from keras.models import Input, Model
-from keras.layers import Conv2D, Concatenate, MaxPooling2D
-from keras.layers import UpSampling2D, Dropout, Activation
+from keras.layers import Conv2D, Concatenate, MaxPooling2D, Reshape
+from keras.layers import UpSampling2D, Dropout, Activation, Permute
 
-def conv_block(m, dim, acti, drop):
-    m = Conv2D(dim, (3, 3), activation=acti, padding='same')(m)
-    if drop != 0: m = Dropout(drop)(m)
-    return Conv2D(dim, (3, 3), activation=acti, padding='same')(m)
-
-def level_block(m, dim, depth, acti, drop):
+def level_block(m, dim, depth, acti):
     if depth > 0:
-        n = conv_block(m, dim, acti, drop)
+        n = Conv2D(dim, (3, 3), activation=acti, padding='same')(m)
+        n = Conv2D(dim, (3, 3), activation=acti, padding='same')(n)
         m = MaxPooling2D((2, 2))(n)
         m = level_block(m, 2*dim, depth-1, acti, drop)
         m = UpSampling2D((2, 2))(m)
+        m = Conv2D(dim, (2, 2), activation=acti, padding='same')(m)
         m = Concatenate(axis=3)([n, m])
-    return conv_block(m, dim, acti, drop)
+        m = Conv2D(dim, (3, 3), activation=acti, padding='same')(m)
+        return Conv2D(dim, (3, 3), activation=acti, padding='same')(m)
 
-def UNet(img_shape, n_out=1, dim=64, depth=4, acti='elu', drop=0, flatten=False):
+def UNet(img_shape, n_out=1, dim=64, depth=4, acti='elu', flatten=False):
     i = Input(shape=img_shape)
     o = level_block(i, dim, depth, acti, drop)
     o = Conv2D(n_out, (1, 1))(o)
