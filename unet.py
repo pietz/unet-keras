e147b0877bc9d8352d1d7e4aa909f4eb392e1eda
Paul-Louis Pröve <mail@plpp.de>
2017-08-10 11:39:56 +0200

    new functions, parameter name changes
    - added residual option
    - added transposed conv option
    - fixed dropout strategy to original u-net
    - 'fcn' is now 'maxpool' and the meaning is inverted
    - 'bn' is now 'batchnorm'

--- unet.py
+++ unet.py
@@ -8,34 +8,44 @@ U-Net: Convolutional Networks for Biomedical Image Segmentation
 ---
 img_shape: (height, width, channels)
 out_ch: number of output channels
-start_ch: number of channels of the first convolution
+start_ch: number of channels of the first conv
 depth: zero indexed depth of the U-structure
-inc_rate: rate at which the convolutional channels will increase
+inc_rate: rate at which the conv channels will increase
 activation: activation function after convolutions
 dropout: amount of dropout in the contracting part
-bn: adds Batch Normalization if true
-fcn: use strided convolutions instead of maxpooling if true
+batchnorm: adds Batch Normalization if true
+maxpool: use strided conv instead of maxpooling if false
+upconv: use transposed conv instead of upsamping + conv if false
+residual: add residual connections around each conv block if true
 '''
 
-def conv_bn(m, dim, acti, bn):
-    m = Conv2D(dim, 3, activation=acti, padding='same')(m)
-    return BatchNormalization()(m) if bn else m
+def conv_block(m, dim, acti, bn, res, do=0):
+	n = Conv2D(dim, 3, activation=acti, padding='same')(m)
+	n = BatchNormalization()(n) if bn else n
+	n = Dropout(do)(n) if do else n
+	n = Conv2D(dim, 3, activation=acti, padding='same')(n)
+	n = BatchNormalization()(n) if bn else n
+	return Concatenate()([m, n]) if res else n
 
-def level_block(m, dim, depth, inc_rate, acti, dropout, bn, fcn):
-    if depth > 0:
-        n = conv_bn(m, dim, acti, bn)
-        n = Dropout(dropout)(n) if dropout else n
-        n = conv_bn(n, dim, acti, bn)
-        m = Conv2D(dim, 3, strides=2, activation=acti, padding='same')(n) if fcn else MaxPooling2D()(n)
-        m = level_block(m, int(inc_rate*dim), depth-1, inc_rate, acti, dropout, bn, fcn)
-        m = UpSampling2D()(m)
-        m = Conv2D(dim, 2, activation=acti, padding='same')(m)
-        m = Concatenate(axis=3)([n, m])
-    m = conv_bn(m, dim, acti, bn)
-    return conv_bn(m, dim, acti, bn)
+def level_block(m, dim, depth, inc, acti, do, bn, mp, up, res):
+	if depth > 0:
+		n = conv_block(m, dim, acti, bn, res)
+		m = MaxPooling2D()(n) if mp else Conv2D(dim, 3, strides=2, padding='same')(n)
+		m = level_block(m, int(inc*dim), depth-1, inc, acti, do, bn, mp, up, res)
+		if up:
+			m = UpSampling2D()(m)
+			m = Conv2D(dim, 2, activation=acti, padding='same')(m)
+		else:
+			m = Conv2DTranspose(dim, 3, strides=2, activation=acti, padding='same')(m)
+		n = Concatenate()([n, m])
+		m = conv_block(n, dim, acti, bn, res)
+	else:
+		m = conv_block(m, dim, acti, bn, res, do)
+	return m
 
-def UNet(img_shape, out_ch=1, start_ch=64, depth=4, inc_rate=2, activation='relu', dropout=0.05, bn=False, fcn=False):
-    i = Input(shape=img_shape)
-    o = level_block(i, start_ch, depth, inc_rate, activation, dropout, bn, fcn)
-    o = Conv2D(out_ch, 1, activation='sigmoid')(o)
-    return Model(inputs=i, outputs=o)
+def UNet(img_shape, out_ch=1, start_ch=64, depth=4, inc_rate=2., activation='relu', 
+		 dropout=0.5, batchnorm=False, maxpool=True, upconv=True, residual=False):
+	i = Input(shape=img_shape)
+	o = level_block(i, start_ch, depth, inc_rate, activation, dropout, batchnorm, maxpool, upconv, residual)
+	o = Conv2D(out_ch, 1, activation='sigmoid')(o)
+	return Model(inputs=i, outputs=o)
